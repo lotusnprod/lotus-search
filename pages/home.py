b6a7350590437c960ea18de7ec0da60c1070933d
Adriano Rutz <adriano.rutz@hotmail.com>
2023-09-24 19:49:52 +0200

    changing logo

--- pages/home.py
+++ pages/home.py
@@ -7,6 +7,6 @@ dash.register_page(__name__, path='/', order=1)
 
 def layout():
     return dbc.Container([
-        html.Img(src=dash.get_asset_url('lotus.png')),
+        html.Img(src='https://upload.wikimedia.org/wikipedia/commons/6/64/Lotus_initiative_logo.svg'),
         dash.dcc.Markdown(open("home.md", "r").read())
     ])
