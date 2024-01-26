b5fdbd2c9fe764c3079481e2d6d961ada673ad86
Adriano Rutz <adriano.rutz@hotmail.com>
2024-01-26 16:47:01 +0100

    Refactor API

--- tests/test_api_structures.py
+++ tests/test_api_structures.py
@@ -1,7 +1,15 @@
 import pytest
 
 from api.api import search_structures
-from api.models import Item
+from api.models import (
+    Filter,
+    Item,
+    Reference,
+    StructuralFilter,
+    Structure,
+    TaxalFilter,
+    Taxon,
+)
 
 from .common import data_model
 
@@ -9,27 +17,30 @@ from .common import data_model
 @pytest.mark.usefixtures("data_model")
 class TestApiStructures:
     async def test_search_structures_pure_structure(self, data_model):
-        item = Item(structure="C", limit=10)
+        item = Item(structure={"molecule": "C"}, filter={"limit": 10})
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
         assert result.structures[3].smiles == "C"
         assert result.description == "Structures matching the query"
 
     async def test_search_error_giving_both_structure_and_wid(self, data_model):
-        item = Item(structure="C", structure_wid="1")
+        item = Item(structure={"molecule": "C", "wid": 1})
         with pytest.raises(Exception):
             await search_structures(item=item, dm=data_model)
 
     async def test_search_invalid_structure(self, data_model):
-        item = Item(structure="C1", limit=10)
+        item = Item(structure={"molecule": "C1"}, filter={"limit": 10})
         with pytest.raises(Exception):
             await search_structures(item=item, dm=data_model)
-        item.substructure_search = True
+        item.filter.structural.substructure_search = True
         with pytest.raises(Exception):
             await search_structures(item=item, dm=data_model)
 
     async def test_search_structures_by_substructure(self, data_model):
-        item = Item(structure="C", substructure_search=True, limit=10)
+        item = Item(
+            structure={"molecule": "C"},
+            filter={"structural": {"substructure_search": True}, "limit": 10},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 4
         assert result.structures[1].smiles == "C[C@H](N)O"
@@ -37,7 +48,10 @@ class TestApiStructures:
         assert result.description == "Structures matching the query"
 
     async def test_search_structures_by_substructure_explicit_h(self, data_model):
-        item = Item(structure="C([H])([H])([H])", substructure_search=True, limit=10)
+        item = Item(
+            structure={"molecule": "C([H])([H])([H])"},
+            filter={"structural": {"substructure_search": True}, "limit": 10},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 3
         assert result.structures[1].smiles == "C[C@H](N)O"
@@ -46,7 +60,8 @@ class TestApiStructures:
 
     async def test_search_structures_by_similarity_explicit_h(self, data_model):
         item = Item(
-            structure="C([H])([H])([H])([H])", substructure_search=False, limit=10
+            structure={"molecule": "C([H])([H])([H])([H])"},
+            filter={"structural": {"substructure_search": False}, "limit": 10},
         )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
@@ -54,13 +69,16 @@ class TestApiStructures:
         assert result.description == "Structures matching the query"
 
     async def test_search_structures_by_substructure_limits(self, data_model):
-        item = Item(structure="C", substructure_search=True)
+        item = Item(
+            structure={"molecule": "C"},
+            filter={"structural": {"substructure_search": True}},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 4
-        item.limit = 0
+        item.filter.limit = 0
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 4
-        item.limit = 1
+        item.filter.limit = 1
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
         assert len(result.structures) == 1
@@ -68,7 +86,9 @@ class TestApiStructures:
 
     async def test_search_structure_restrict_taxon_exists(self, data_model):
         # We search for a compound that exist in this taxon
-        item = Item(structure="CC(N)O", taxon_wid=1, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"}, taxon={"wid": 1}, filter={"limit": 10}
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
         assert result.structures[1].smiles == "C[C@H](N)O"
@@ -76,27 +96,31 @@ class TestApiStructures:
 
     async def test_search_structure_restrict_taxon_not_exists(self, data_model):
         # We search for a compound that does not exist in this taxon
-        item = Item(structure="CC(N)O", taxon_wid=3, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"}, taxon={"wid": 3}, filter={"limit": 10}
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 0
 
     async def test_search_structure_restrict_reference_exists(self, data_model):
         # We search for a compound that exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=1, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"}, reference={"wid": 1}, filter={"limit": 10}
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
         assert result.structures[1].smiles == "C[C@H](N)O"
         assert result.description == "Structures matching the query"
 
     async def test_search_structure_restrict_reference_not_matching(self, data_model):
-        # We search for a compound that does not exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=4)
+        # We search for a compound that does not exist in this ref
+        item = Item(structure={"molecule": "CC(N)O"}, reference={"wid": 4})
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 0
 
     async def test_search_structure_restrict_reference_not_existing(self, data_model):
-        # We search for a compound that does not exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=666)
+        # We search for a compound that does not exist in this ref
+        item = Item(structure={"molecule": "CC(N)O"}, reference={"wid": 666})
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 0
 
@@ -104,7 +128,12 @@ class TestApiStructures:
         self, data_model
     ):
         # We search for a compound that exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=1, taxon_wid=1, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"},
+            reference={"wid": 1},
+            taxon={"wid": 1},
+            filter={"limit": 10},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 1
         assert result.structures[1].smiles == "C[C@H](N)O"
@@ -114,7 +143,12 @@ class TestApiStructures:
         self, data_model
     ):
         # We search for a compound that exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=4, taxon_wid=3, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"},
+            reference={"wid": 4},
+            taxon={"wid": 3},
+            filter={"limit": 10},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 0
 
@@ -122,6 +156,11 @@ class TestApiStructures:
         self, data_model
     ):
         # We search for a compound that exist in this taxon
-        item = Item(structure="CC(N)O", reference_wid=1, taxon_wid=3, limit=10)
+        item = Item(
+            structure={"molecule": "CC(N)O"},
+            reference={"wid": 1},
+            taxon={"wid": 3},
+            filter={"limit": 10},
+        )
         result = await search_structures(item=item, dm=data_model)
         assert result.count == 0
--- tests/test_api_triplets.py
+++ tests/test_api_triplets.py
@@ -1,7 +1,16 @@
 import pytest
 
 from api.api import search_triplets
-from api.models import Item, TripletResult
+from api.models import (
+    Filter,
+    Item,
+    Reference,
+    StructuralFilter,
+    Structure,
+    TaxalFilter,
+    Taxon,
+    TripletResult,
+)
 
 from .common import data_model
 
@@ -18,15 +27,15 @@ class TestApiTriplets:
         assert result.description == "Triplets matching the query"
 
     async def test_triplets_limits(self, data_model):
-        item = Item(limit=0)
+        item = Item(filter={"limit": 0})
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 6
-        item.limit = 3
+        item.filter.limit = 3
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 3
 
     async def test_triplets_one_reference(self, data_model):
-        item = Item(reference_wid=3)
+        item = Item(reference={"wid": 3})
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 2
         assert len(result.references) == 1
@@ -35,7 +44,7 @@ class TestApiTriplets:
         assert result.description == "Triplets matching the query"
 
     async def test_triplets_one_reference_one_compound(self, data_model):
-        item = Item(reference_wid=3, structure_wid=2)
+        item = Item(reference={"wid": 3}, structure={"wid": 2})
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 1
         assert len(result.references) == 1
@@ -44,7 +53,7 @@ class TestApiTriplets:
         assert result.description == "Triplets matching the query"
 
     async def test_triplets_one_reference_one_non_existing_compound(self, data_model):
-        item = Item(reference_wid=3, structure_wid=666)
+        item = Item(reference={"wid": 3}, structure={"wid": 666})
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 0
         assert len(result.references) == 0
@@ -53,7 +62,7 @@ class TestApiTriplets:
         assert result.description == "Triplets matching the query"
 
     async def test_triplets_one_taxon(self, data_model):
-        item = Item(taxon_wid=1)
+        item = Item(taxon={"wid": 1})
         result: TripletResult = await search_triplets(item=item, dm=data_model)
         assert result.count == 3
         assert len(result.references) == 2
