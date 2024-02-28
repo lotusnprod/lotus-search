64a1c3256adaf2bba28ad62d14058d22e173126f
Adriano Rutz <adriano.rutz@hotmail.com>
2024-02-28 09:25:06 +0100

    WIP #81

--- storage/models/references.py
+++ storage/models/references.py
@@ -9,12 +9,19 @@ class References(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     doi: Mapped[str]
+    # TODO uncomment when ready
+    # title: Mapped[str]
+    # date: Mapped[str]
+    # journal: Mapped[int]
 
     __table_args__ = (
-        # TODO add other fields
         Index("reference_doi", "doi"),
         Index("reference_id", "id"),
+        # Index("reference_title", "title"),
+        # Index("reference_date", "date"),
+        # Index("reference_journal", "journal"),
     )
 
     def __repr__(self):
         return f"References(id={self.id}, doi={self.doi})"
+        # return f"References(id={self.id}, doi={self.doi}, title={self.title}, date={self.date}, journal={self.journal})"
